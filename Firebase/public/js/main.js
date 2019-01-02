fde5361a1f3679d331e46beac6f5d480fa98dae3
hydra1988 <hydra1988@naver.com>
2019-01-02 17:44:48 +0900

    init2

--- Firebase/public/js/main.js
+++ Firebase/public/js/main.js
@@ -12,7 +12,15 @@ var menu04 = document.getElementById("menu04");
 menu01.innerHTML = "악보";
 menu02.innerHTML = "코드학습";
 menu03.innerHTML = "아이도르";
-menu04.innerHTML = "아직몰랑";
+menu04.innerHTML = "테스트용";
+menu05.innerHTML = "영어학습";
+menu06.innerHTML = "경제";
+
+// 링크 
+document.getElementById("menu01").setAttribute("href", "index.html" );
+document.getElementById("menu02").setAttribute("href" , "02.code.html");
+document.getElementById("menu03").setAttribute("href" , "03.idol.html");
+document.getElementById("menu04").setAttribute("href" , "test.html");
 
 
 
@@ -22,7 +30,7 @@ menu04.innerHTML = "아직몰랑";
  * 
  */
 
-var active = ["01","02","03","04"];
+var active = ["01","02","03","04","05","06"];
 
 for(var i = 0 ; i<active.length; ++i)
 {
@@ -39,10 +47,7 @@ document.getElementById("active" + active[i]).onmouseout = function ()
 }
 
 
-document.getElementById("menu01").setAttribute("href", "index.html" );
-document.getElementById("menu02").setAttribute("href" , "02.code.html");
-document.getElementById("menu03").setAttribute("href" , "03.idol.html");
-document.getElementById("menu04").setAttribute("href" , "test.html");
+
 
 
 /************************************ */
